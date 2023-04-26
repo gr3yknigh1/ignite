7fb66984cdfe7a300b2a305c658f0fc4fe372f54
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-04-26 10:06:24 +0300

    Check node to be null

--- src/link_node.c
+++ src/link_node.c
@@ -1,8 +1,14 @@
 #include "ignite/link_node.h"
 
+#include <stddef.h>
+
 void ignite_link_node_init(struct ignite_link_node *node, void *data,
                            struct ignite_link_node *next,
                            struct ignite_link_node *prev) {
+    if (node == NULL) {
+        return;
+    }
+
     *node = (struct ignite_link_node) {
         .data = data,
         .next = next,
