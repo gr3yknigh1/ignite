68e1c2fdd7817f557dfb2245dab9c2e52a2d2138
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-05-03 16:21:05 +0300

    Add smart cleanup macros

--- engine/include/ignite/memory.h
+++ engine/include/ignite/memory.h
@@ -4,11 +4,16 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+#include "ignite/ignite.h"
+
 void *ignite_memory_allocate(size_t bytes);
 
 void *ignite_memory_copy(void *destanation, const void *restrict source,
                          size_t bytes);
 
 void ignite_memory_free(void *pointer);
+
+#define smart_free IGNITE_CLEANUP(ignite_memory_free)
+#define smart(__clean_callback) IGNITE_CLEANUP(__clean_callback)
 
 #endif // IGNITE_MEMORY_H_
