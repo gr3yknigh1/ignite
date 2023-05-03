47b55c6c3fd6d9f232739bf210d2cb4f3cfda1fe
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-05-03 14:07:43 +0300

    Add numtypes typedefs

---
+++ engine/include/ignite/numtypes.h
@@ -0,0 +1,24 @@
+#ifndef IGNITE_NUMTYPES_H_
+#define IGNITE_NUMTYPES_H_
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <sys/types.h>
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long u64;
+
+typedef signed char i8;
+typedef signed short i16;
+typedef signed int i32;
+typedef signed long i64;
+
+typedef size_t usize;
+typedef ssize_t ssize;
+
+typedef unsigned char byte;
+typedef const char *cstr;
+
+#endif // IGNITE_NUMTYPES_H_
